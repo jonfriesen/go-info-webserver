e5fd4be41066295f27bf4a1ecfea12c7a9189391
Jon Friesen <jfriesen@digitalocean.com>
2023-02-02 09:17:01 -0700

    adds upload endpoint

--- main.go
+++ main.go
@@ -62,6 +62,7 @@ func main() {
 
 		fmt.Fprintln(w, v)
 	})
+	r.HandleFunc("/upload", uploadFile)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -256,4 +257,88 @@ func testPostgresConnection(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, "Successfully connected and pinged postgres.")
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Println("get method hit, return html")
+		fmt.Fprint(w, getUploadForm())
+	}
+	// guard non-post methods
+	if r.Method != http.MethodPost {
+		fmt.Fprintf(w, "Method %s not supported.", r.Method)
+		return
+	}
+
+	fmt.Println("File Upload Endpoint Hit")
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile("", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// write this byte array to our temporary file
+	_, err = tempFile.Write(fileBytes)
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+// i am lazy
+func getUploadForm() string {
+	return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+  </head>
+  <body>
+    <form
+      enctype="multipart/form-data"
+      action="/upload"
+      method="POST"
+    >
+      <input type="file" name="myFile" multiple/>
+      <input type="submit" value="upload" />
+    </form>
+  </body>
+</html>
+	`
 }
